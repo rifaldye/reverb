b5876c074307cbeb86fc2ff02777213c65b04232
rifaldye <rifaldyelninoru@gmail.com>
2025-04-30 16:25:25 +0700

    feat: custom jwt auth for private channel

--- src/Protocols/Pusher/Channels/Concerns/InteractsWithPrivateChannels.php
+++ src/Protocols/Pusher/Channels/Concerns/InteractsWithPrivateChannels.php
@@ -2,6 +2,9 @@
 
 namespace Laravel\Reverb\Protocols\Pusher\Channels\Concerns;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Laravel\Reverb\Contracts\Connection;
 use Laravel\Reverb\Protocols\Pusher\Exceptions\ConnectionUnauthorized;
@@ -23,10 +26,24 @@ trait InteractsWithPrivateChannels
      */
     protected function verify(Connection $connection, ?string $auth = null, ?string $data = null): bool
     {
-         if($auth !== 'connect'){
-            return false;
+        //validate auth is valid jwt token
+        $key = $connection->app()->secret();
+        if (!is_string($key)) {
+            throw new ConnectionUnauthorized;
         }
 
+        try{
+            $decoded = JWT::decode($auth, new Key($key, 'HS256'));
+        }catch(\Exception $e){
+            throw new ConnectionUnauthorized;
+        }
+
+// [2025-04-30 17:18:47] staging.INFO: {"name":"john","access":["stockPrice"],"exp":1746004911}
+        $channelName = explode('.', $this->name)[0];
+        $channelName = explode('-', $channelName)[1];
+        if(!in_array($channelName, $decoded->access)){
+            throw new ConnectionUnauthorized;
+        }
         return true;
     }
 }
