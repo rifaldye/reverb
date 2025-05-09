b4b4b5686a459fc718bfb9d7bb2ff0841773749f
rifaldye <rifaldyelninoru@gmail.com>
2025-05-09 19:50:04 +0700

    feat: add custom verify logic

--- src/Protocols/Pusher/Channels/Concerns/InteractsWithPrivateChannels.php
+++ src/Protocols/Pusher/Channels/Concerns/InteractsWithPrivateChannels.php
@@ -2,13 +2,12 @@
 
 namespace Laravel\Reverb\Protocols\Pusher\Channels\Concerns;
 
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Laravel\Reverb\Contracts\Connection;
 use Laravel\Reverb\Protocols\Pusher\Exceptions\ConnectionUnauthorized;
-
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Illuminate\Support\Facades\Log;
 trait InteractsWithPrivateChannels
 {
     /**
@@ -24,8 +23,8 @@ trait InteractsWithPrivateChannels
     /**
      * Determine whether the given authentication token is valid.
      */
-    protected function verify(Connection $connection, ?string $auth = null, ?string $data = null): bool
-    {
+
+     protected function customVerify(Connection $connection, ?string $auth = null, ?string $data = null){
         //validate auth is valid jwt token
         $key = $connection->app()->secret();
         if (!is_string($key)) {
@@ -44,6 +43,27 @@ trait InteractsWithPrivateChannels
         if(!in_array($channelName, $decoded->access)){
             throw new ConnectionUnauthorized;
         }
+        return true;
+     }
+    protected function verify(Connection $connection, ?string $auth = null, ?string $data = null): bool
+    {
+        $signature = "{$connection->id()}:{$this->name()}";
+
+        if ($data) {
+            $signature .= ":{$data}";
+        }
+
+        if (! hash_equals(
+            hash_hmac(
+                'sha256',
+                $signature,
+                $connection->app()->secret(),
+            ),
+            Str::after($auth, ':')
+        )) {
+            return $this->customVerify($connection, $auth, $data);
+        }
+
         return true;
     }
 }
