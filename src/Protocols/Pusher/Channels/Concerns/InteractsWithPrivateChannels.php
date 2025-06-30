5a7f430cde46d0f2f57db659eff33382c0a8840d
rifaldye <rifaldyelninoru@gmail.com>
2025-06-30 15:11:50 +0700

    feat: update logic for perps

--- src/Protocols/Pusher/Channels/Concerns/InteractsWithPrivateChannels.php
+++ src/Protocols/Pusher/Channels/Concerns/InteractsWithPrivateChannels.php
@@ -37,17 +37,14 @@ trait InteractsWithPrivateChannels
             throw new ConnectionUnauthorized;
         }
 
-// [2025-04-30 17:18:47] staging.INFO: {"name":"john","access":["stockPrice"],"exp":1746004911}
         $channelDecode = explode('.', $this->name);
         $channelName = explode('-', $channelDecode[0])[1];
         if(!in_array($channelName, $decoded->access)){
             throw new ConnectionUnauthorized;
         }
-
         if($channelName == 'userPerps' && $channelDecode[2] != $decoded->userPerps){
             throw new ConnectionUnauthorized;
         }
-
         return true;
      }
     protected function verify(Connection $connection, ?string $auth = null, ?string $data = null): bool
